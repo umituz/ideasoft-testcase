65ab3127f89e76ff059aa242debb4623d7afe7d3
Ümit UZ <umituz998@gmail.com>
2024-01-28 20:04:25 +0300

    register integration tests

---
+++ tests/Feature/Http/Controllers/Api/Auth/RegisterControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterControllerTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function test_can_register_user()
+    {
+        $userData = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => 'password123',
+            'password_confirmation' => 'password123',
+        ];
+
+        $response = $this->postJson('/api/auth/register', $userData);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('users', ['email' => $userData['email']]);
+
+        $response->assertJsonFragment(['email' => $userData['email']]);
+    }
+}
